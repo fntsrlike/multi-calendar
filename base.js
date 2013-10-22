9f0f503cd5dfec8caae2ff453fca87f2a975d2f6
fntsrlike <fntsrlike@gmail.com>
2013-10-22 08:10:19 +0800

    add choose/cancel all feature;

--- base.js
+++ base.js
@@ -1,15 +1,17 @@
-$('.btn').click(function() {
+$('.cal').click(function() {
 
   var src = $('#calendar').attr('src');
   src = src.replace('ctz=Asia/Taipei', '');
   
-  if ($(this).hasClass('active')) {
+  if ($(this).hasClass('btn-success')) {
     src = src.replace($(this).attr('src'), '');
-    $(this).removeClass('active');
+    $(this).removeClass('btn-success');
+    $(this).addClass('btn-default');  
   }
   else {
     src = src + $(this).attr('src') + '&';
-    $(this).addClass('active');  
+    $(this).removeClass('btn-default');
+    $(this).addClass('btn-success');  
   }
   
   src = src + 'ctz=Asia/Taipei';
@@ -17,4 +19,37 @@ $('.btn').click(function() {
   $('#calendar').attr('src',src);
   
   
+});
+
+
+$('#cancel_all').click(function() {
+  $('#calendar').attr('src','https://www.google.com/calendar/embed?ctz=Asia/Taipei');
+
+  $('.cal').each(function(){
+    if ($(this).hasClass('btn-success')) {
+      $(this).removeClass('btn-success');
+      $(this).addClass('btn-default');  
+    } 
+  });
+
+});
+
+$('#choose_all').click(function() {
+  
+  var src = 'https://www.google.com/calendar/embed?ctz=Asia/Taipei'
+  src = src.replace('ctz=Asia/Taipei', '');
+  
+  $('.cal').each(function(){
+    if ($(this).hasClass('btn-default')) {
+      $(this).removeClass('btn-default');
+      $(this).addClass('btn-success');  
+    }  
+
+    src = src + $(this).attr('src') + '&';
+  });
+  
+  
+  src = src + 'ctz=Asia/Taipei';
+  
+  $('#calendar').attr('src',src);
 });
